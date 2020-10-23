06ac7eb51a00b34070d3cc19754face14ff8d06c
PhyntomOne <61550895+PHYNTOM1@users.noreply.github.com>
2020-10-23 17:11:21 +0200

    added basic scuffed cart

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartController : MonoBehaviour
+{
+
+    public WheelCollider[] wheelColls;
+    public GameObject[] wheelMeshs;
+    
+    [Space]
+    public Transform massCenter;
+
+    public float maxMotorTorque;
+    public float maxSteering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = massCenter.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteering * Input.GetAxis("Horizontal");
+
+        foreach (WheelCollider wc in wheelColls)
+        {
+            wc.steerAngle = steering;
+            wc.motorTorque = motor;
+
+        }
+
+        CollToMeshRotation();
+    }
+
+
+
+    public void CollToMeshRotation()
+    {
+        Quaternion rot;
+        Vector3 pos;
+
+        for (int i = 0; i < wheelColls.Length; i++)
+        {
+            wheelColls[i].GetWorldPose( out pos, out rot);
+            //wheelMeshs[i].transform.position = pos;
+            //wheelMeshs[i].transform.rotation = rot;
+            wheelMeshs[i].transform.eulerAngles = new Vector3(0, rot.eulerAngles.y, 90);
+        }
+    }
+}
