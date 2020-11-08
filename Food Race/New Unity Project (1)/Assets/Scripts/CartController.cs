b1883743bd37a78b81d353e45c76fdbace9472e8
ahmudie <60074291+ahmudie@users.noreply.github.com>
2020-11-08 23:00:45 +0100

    New Script and changes for Collider
    New CartController Script

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180;
+
+    public float speedInput, turnInput;
+
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnInput = Input.GetAxis("Horizontal");
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+
+
+
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        }
+
+        transform.position = theRB.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        theRB.AddForce(transform.forward * speedInput);
+    }
+}
