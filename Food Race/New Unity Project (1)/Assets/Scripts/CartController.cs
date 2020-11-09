543aa8884faa58de75f58bd5b693830fb178c6fd
PhyntomOne <61550895+PHYNTOM1@users.noreply.github.com>
2020-11-09 18:58:45 +0100

    kinda heavy cart driving

--- Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
+++ Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
@@ -6,7 +6,7 @@ public class CartController : MonoBehaviour
 {
     public Rigidbody theRB;
 
-    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f;
+    public float forwardAccel = 1f, reverseAccel = 0.66f, maxSpeed = 8f, turnStrength = 120, gravityForce = 10f;
 
     public float speedInput, turnInput;
 
@@ -31,28 +31,86 @@ public class CartController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        turnInput = Input.GetAxis("Horizontal");
 
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
+        {
+            turnInput = Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            if (turnInput <= 1f && turnInput >= 0.1f)
+            {
+                turnInput -= 0.1f;
+            }
+            else if (turnInput >= -1f && turnInput <= -0.1f)
+            {
+                turnInput += 0.1f;
+            }
+            else
+            {
+                turnInput = 0;
+            }
+        }
 
         if (grounded)
         {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+            if (speedInput <= 3000 && speedInput >= 0 || speedInput >= -3000 && speedInput <= 0)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * 0.1f, 0f));
+            }
+            else if (speedInput <= 5000 && speedInput >= 0 || speedInput >= -5000 && speedInput <= 0)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * 0.2f, 0f));
+            }
+            else if (speedInput <= 7000 && speedInput >= 0 || speedInput >= -7000 && speedInput <= 0)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * 0.3f, 0f));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * 0.4f, 0f));
+            }
         }
 
         leftfrontwheel.localRotation = Quaternion.Euler(leftfrontwheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftfrontwheel.localRotation.eulerAngles.z);
         rightfrontwheel.localRotation = Quaternion.Euler(rightfrontwheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightfrontwheel.localRotation.eulerAngles.z);
 
 
-        speedInput = 0f;
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        //speedInput = 0f;
 
-        }
-        else if(Input.GetAxis("Vertical") < 0)
+        if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                if (speedInput < maxSpeed * 1000f && speedInput >= 0f)
+                {
+                    speedInput += forwardAccel * maxSpeed * 10f;
+                }
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                if (speedInput > -maxSpeed * 1000f && speedInput <= 0f)
+                {
+                    speedInput -= forwardAccel * maxSpeed * 20f;
+                }
+            }
         }
+        else
+        {
+            if (speedInput <= maxSpeed * 1000f && speedInput >= maxSpeed * 50f)
+            {
+                speedInput -= 50f;
+            }
+            else if (speedInput >= -maxSpeed * 1000f && speedInput <= -maxSpeed * 50f)
+            {
+                speedInput += 50f;
+            }
+            else
+            {
+                speedInput = 0;
+            }
+        }
+
 
         transform.position = theRB.transform.position;
     }
