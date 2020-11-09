84c71ec0b4abb6e40fabc7497b99e881c243d5ff
ahmudie <60074291+ahmudie@users.noreply.github.com>
2020-11-09 17:40:17 +0100

    implemented Driftsystem and gravity

--- Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
+++ Food Race/New Unity Project (1)/Assets/Scripts/CartController.cs
@@ -6,9 +6,22 @@ public class CartController : MonoBehaviour
 {
     public Rigidbody theRB;
 
-    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180;
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f;
 
     public float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask whatIsGround;
+    public float groundRayLenght = .5f;
+    public Transform groundRayPoint;
+
+    public Transform leftfrontwheel, rightfrontwheel;
+    public float maxWheelTurn = 25f;
+
+    public ParticleSystem[] exhaust;
+    public float maxEmission = 25f;
+    private float emissionRate;
 
     void Start()
     {
@@ -19,8 +32,15 @@ public class CartController : MonoBehaviour
     void Update()
     {
         turnInput = Input.GetAxis("Horizontal");
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
 
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+
+        leftfrontwheel.localRotation = Quaternion.Euler(leftfrontwheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftfrontwheel.localRotation.eulerAngles.z);
+        rightfrontwheel.localRotation = Quaternion.Euler(rightfrontwheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightfrontwheel.localRotation.eulerAngles.z);
 
 
         speedInput = 0f;
@@ -39,6 +59,32 @@ public class CartController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        theRB.AddForce(transform.forward * speedInput);
+        grounded = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLenght, whatIsGround))
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        emissionRate = 0;
+
+        if (grounded)
+        {
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                theRB.AddForce(transform.forward * speedInput);
+
+                emissionRate = maxEmission;
+            }
+        }
+        else
+        {
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
+        }
+        
+        
     }
 }
