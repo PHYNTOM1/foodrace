cc8cf036f282eef616247a2673fe298566333a5c
ahmudie <60074291+ahmudie@users.noreply.github.com>
2020-12-01 02:27:15 +0100

    Countdown is implemented

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 3f;
+
+    public Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            countdownText.text = "START";
+            
+        }
+        if (currentTime <= -1)
+        {
+            countdownText.text = " ";
+        }
+    }
+}
