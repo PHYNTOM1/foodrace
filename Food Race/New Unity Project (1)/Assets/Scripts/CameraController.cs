dd90961e91d43bcf10f4f71e848a85348af5f65a
ahmudie <60074291+ahmudie@users.noreply.github.com>
2021-01-19 02:02:52 +0100

    Cam Controller
    Following Camera was implemented

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed = 10.0f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    void FixedUpdate()
+    {
+        Vector3 dpos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dpos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+}
+
