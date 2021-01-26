646c560334d7c067ee3b08c20899cb6abf77c454
ahmudie <60074291+ahmudie@users.noreply.github.com>
2021-01-26 06:14:38 +0100

    added working simple minimap

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/MinimapCamFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.eulerAngles = new Vector3(90f, player.transform.eulerAngles.y, 0f);
+    }
+}
