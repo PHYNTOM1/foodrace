8d369f694a0231fe3b2d0cbf4942190aecc7ad21
ahmudie <60074291+ahmudie@users.noreply.github.com>
2021-01-27 05:21:28 +0100

    added 3d menu ui (menus and ranking screen)

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/World3DCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World3DCanvas : MonoBehaviour
+{
+    public bool rotating = false;
+
+    public Transform camTransform;
+    Quaternion originalRotation;
+
+    void Start()
+    {
+        GameObject cam = GameObject.Find("UI Camera");
+
+        GetComponent<Canvas>().worldCamera = cam.GetComponent<Camera>();
+        camTransform = cam.transform;
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (rotating)
+        {
+            transform.rotation = camTransform.rotation * originalRotation;
+        }
+    }
+}
