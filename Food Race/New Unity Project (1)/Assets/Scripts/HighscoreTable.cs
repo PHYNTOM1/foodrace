ce8a692b8e6e134dd78771652aea1d6b10a2e105
ahmudie <60074291+ahmudie@users.noreply.github.com>
2021-01-28 02:57:55 +0100

    working on Ranklist

---
+++ Food Race/New Unity Project (1)/Assets/Scripts/HighscoreTable.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+<<<<<<< Updated upstream
+using UnityEngine.UI;
+using TMPro;
+
+
+public class HighscoreTable : MonoBehaviour
+{
+    public Transform entryContainer;
+    public Transform entryTemplate;
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
+
+
+
+
+    public int amount = 0;
+
+    public void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        highscoreEntryList = new List<HighscoreEntry>()
+        {
+            new HighscoreEntry{score = 52123231, name = "AAA"},
+            new HighscoreEntry{score = 528723231, name = "AVA"},
+            new HighscoreEntry{score = 8971389, name = "CAA"},
+            new HighscoreEntry{score = 70910898, name = "ABC"},
+            new HighscoreEntry{score = 52231, name = "AHA"},
+
+        };
+
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+
+
+
+    }
+   
+
+            
+        
+      
+        private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+        {
+
+            float templateHeight = 20f;
+            Transform entryTransform = Instantiate(entryTemplate, container);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+            entryTransform.gameObject.SetActive(true);
+        
+        
+            int rank = transformList.Count + 1;
+            string rankString;
+        
+        
+            switch (rank)
+            {
+                default:
+                 rankString = rank + "TH"; break;
+
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+
+
+
+            entryTransform.Find("posText").gameObject.GetComponent<Text>().text = rankString;
+
+            int score = highscoreEntry.score;
+
+            entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+
+            string name = highscoreEntry.name;
+
+            entryTransform.Find("nameText").gameObject.GetComponent<Text>().text = name;
+
+            transformList.Add(entryTransform);
+        }
+
+    
+        private class HighscoreEntry
+        {
+            public int score;
+            public string name;
+        }
+
+=======
+
+public class HighscoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = transform.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 20f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+
+    }
+>>>>>>> Stashed changes
+}
