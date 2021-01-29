08afe78665a58d708febdbf735fd1d5ef05c98da
PhyntomOne <61550895+PHYNTOM1@users.noreply.github.com>
2021-01-29 07:23:25 +0100

    shaders rendering project settings test

--- Food Race/New Unity Project (1)/Assets/Scripts/CameraController.cs
+++ Food Race/New Unity Project (1)/Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
@@ -11,7 +12,8 @@ public class CameraController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (cameraTarget = null) {
+        if (SceneManager.GetActiveScene().name == "Ingame2")
+        {
             Vector3 dpos = cameraTarget.position + dist;
             Vector3 sPos = Vector3.Lerp(transform.position, dpos, sSpeed * Time.deltaTime);
             transform.position = sPos;
